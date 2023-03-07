bfe60929cc6403bd89718003c759aeffc4e3b23e
Mow <hoang-vo@ecombest.com>
2023-03-07 22:46:02 +0700

    fix git action

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = (env, argv) => {
-  const isProd = argv.mode === "production";
+const branchName = process.env.BRANCH_NAME || "dev";
+const filename =
+  branchName === "dev" ? "dev.personalization.js" : "stable.personalization.js";
 
-  return {
-    entry: "./src/index.js", // Dẫn tới file index.js ta đã tạo
-    output: {
-      path: path.join(__dirname, "/build"), // Thư mục chứa file được build ra
-      filename: isProd ? "personalization.js" : "dev.personalization.js", // Tên file được build ra
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/, // Sẽ sử dụng babel-loader cho những file .js
-          exclude: /node_modules/, // Loại trừ thư mục node_modules
-          use: ["babel-loader"],
-        },
-        {
-          test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-    // Chứa các plugins sẽ cài đặt trong tương lai
-    plugins: [
-      new webpack.ProvidePlugin({
-        React: "react",
-      }),
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Sẽ sử dụng babel-loader cho những file .js
+        exclude: /node_modules/, // Loại trừ thư mục node_modules
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
+        use: ["style-loader", "css-loader"],
+      },
     ],
-  };
+  },
+  // Chứa các plugins sẽ cài đặt trong tương lai
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
 };
